f0d52871f7efcbff9cbdcdd673c0953fc067c8ac
Adam Orr <adamorr234@gmail.com>
2019-03-18 20:35:34 +0000

    Functional

--- routes/api.js
+++ routes/api.js
@@ -7,35 +7,124 @@
 */
 
 'use strict';
-
+require('dotenv').config();
 var expect = require('chai').expect;
-var MongoClient = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
+function connect(callback) {
+   MongoClient.connect(process.env.DB, function(err, db) {
+  if(err) console.error(err);
+  callback(db);
+  console.log("Server connected");
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
-
+});
+}
+ 
 module.exports = function (app) {
 
-  app.route('/api/issues/:project')
+
+
+      app.route('/api/issues/:project')
   
     .get(function (req, res){
       var project = req.params.project;
+      var queries = req.query;
+      if(project) {
+        connect((db) => {
+      
+          db.collection(project).find(req.query).toArray(function(err, results) {
+            if(err) console.error(err);
+            res.send(results);
+          })
+        })
+      }
+     else {
+       res.status(500).send("cannot find project issue colection name in request query url")
+     }
       
     })
     
-    .post(function (req, res){
+    .post(function (req, res) {
       var project = req.params.project;
+      var data = req.body;
+      
+      if(project && data.issue_title && data.issue_text && data.created_by) {
+        var issueData;
+        var date = new Date();
+        connect((db) => {
+           db.collection(project).findOne({issue_title: data.issue_title}, function(err, issue) {
+          if(err) console.error(err);
+          if(issue) {
+            res.status(500).send("issue already exists");
+          } else {
+            issueData = req.body;
+            issueData.created_on=date.toString();
+            issueData.updated_on = date.toString();
+            issueData.open=data.open ? data.open : true;
+            issueData.assigned_to=data.assigned_to ? data.assigned_to : "";
+            issueData.status_text=data.status_text ? data.status_text : "";
+            db.collection(project).insertOne(issueData, function(err, issue) {
+              if(err) console.error(err);
+              res.send(issue.ops);
+            })
+          }
+        })
+        })
+        
+       
+      } else {
+        res.status(500).send("cannot find one of the required keys for request body: issue_title, issue_text, created_by")
+      }
       
     })
     
     .put(function (req, res){
       var project = req.params.project;
+      var data = req.body || {};
+      var issueData = {};
+      if(Object.keys(data).length <= 1) {
+        res.send("no updated field sent");
+      }
+      else if(project && data._id) {
+        for(const key in data) {
+          issueData[key] = data[key];
+        }
+        issueData.updated_on = new Date().toString();
+        connect((db) => {
+          db.collection(project).updateOne({_id: data._id}, {$set: issueData}, {upsert: true}, function(err, issue) {
+          if(err){
+            console.error(err);
+            res.status(500).send("could not update " + data._id)
+          } else {
+            res.send("successfully updated");
+          }
+          
+        })
+        })
       
+      }
+      else {
+        res.status(500).send("cannot find project collection name in url params and/or _id in request body");
+      }
     })
     
     .delete(function (req, res){
       var project = req.params.project;
-      
-    });
-    
+      if(project && req.body._id) {
+        connect((db) => {
+           db.collection(project).deleteOne({_id: req.body._id}, function(err, db_res) {
+          if(err) {
+            console.error(err);
+            res.status(500).send("could not delete " + req.body._id)
+          } else {
+            res.send("deleted " +req.body._id)
+          }
+        })
+        })
+       
+      }
+      else {
+        res.send("_id error")
+      }
+    }); 
 };
--- server.js
+++ server.js
@@ -4,18 +4,18 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
-
+var helmet = require("helmet");
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
-
+require('dotenv').config();
 var app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
 
-
+app.use(helmet())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
